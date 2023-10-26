55e67ba0f6c5ec0e6769cd507968392a1ced58ea
OmarENNEDDI <enneddiomar@gmail.com>
2023-10-26 23:54:37 +0100

    ajout du code drop down faq et reformulation des dossier du brief

--- Html-Css/JS/Advertise.js
+++ Html-Css/JS/Advertise.js
@@ -3,10 +3,21 @@ const btn = document.querySelector(".menu_btn");
 
 // console.log(btn);
 
-btn.addEventListener('click',function () {
-    menu.classList.toggle('active');
+btn.addEventListener("click", function () {
+  menu.classList.toggle("active");
 
+  console.log("open");
+});
 
-
-    console.log('open');
+const FAQ = document.querySelectorAll("FAQ");
+  FAQ.forEach( function (item) {
+  item.addEventListener("click", function () {
+    item.classList.toggle("active");
+    let para = item.nextElementSibling;
+    if (para.style.display === "block") {
+      para.style.display = "none";
+    } else {
+      para.style.display = "block";
+    }
+  });
 });
